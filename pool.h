a917f51703afcceb8e63acd70fca734c90668c74
y-asaba <y-asaba>
2007-07-24 09:02:42 +0000

    Detect deadlock in do_command(). When insert_lock was true, kind mismatch error occured.

--- pool.h
+++ pool.h
@@ -1,7 +1,7 @@
 /* -*-pgsql-c-*- */
 /*
  *
- * $Header: /cvsroot/pgpool/pgpool-II/pool.h,v 1.9 2007/07/23 04:37:35 y-asaba Exp $
+ * $Header: /cvsroot/pgpool/pgpool-II/pool.h,v 1.10 2007/07/24 09:02:42 y-asaba Exp $
  *
  * pgpool: a language independent connection pool server for PostgreSQL 
  * written by Tatsuo Ishii
@@ -64,11 +64,12 @@
 #define NO_STRICT_MODE(s) (strncasecmp((s), NO_STRICT_MODE_STR, strlen(NO_STRICT_MODE_STR)) == 0)
 
 typedef enum {
-    POOL_CONTINUE = 0,
+	POOL_CONTINUE = 0,
 	POOL_IDLE,
-    POOL_END,
-    POOL_ERROR,
-    POOL_FATAL
+	POOL_END,
+	POOL_ERROR,
+	POOL_FATAL,
+	POOL_DEADLOCK
 } POOL_STATUS;
 
 
