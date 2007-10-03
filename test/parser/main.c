fe08e7175397c4a6d49e4bd1a0478b7dda17f978
y-asaba <y-asaba>
2007-10-03 04:47:18 +0000

    Add SQL parser test.

---
+++ test/parser/main.c
@@ -0,0 +1,40 @@
+/* $Header: /cvsroot/pgpool/pgpool-II/test/parser/main.c,v 1.1 2007/10/03 04:47:18 y-asaba Exp $ */
+#include "pool.h"
+#include "pool_memory.h"
+#include "parsenodes.h"
+#include "gramparse.h"
+#include "parse.h"
+#include "parser.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	List *tree;
+	ListCell *l;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "./parser-test query\n");
+		exit(1);
+	}
+
+	tree = raw_parser(argv[1]);
+
+	if (tree == NULL)
+	{
+		printf("syntax error: %s\n", argv[1]);
+	}
+	else
+	{
+		foreach(l, tree)
+		{
+			Node *node = (Node *) lfirst(l);
+			printf("%s\n", nodeToString(node));
+		}
+	}
+
+	free_parser();
+	return 0;
+}
