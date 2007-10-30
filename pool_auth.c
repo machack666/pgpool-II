be029021e91085d9f65527c7f4898329c81e9ff7
y-asaba <y-asaba>
2007-10-30 02:11:16 +0000

    Previous commit did not support V2 protocol.

--- pool_auth.c
+++ pool_auth.c
@@ -1,6 +1,6 @@
 /* -*-pgsql-c-*- */
 /*
- * $Header: /cvsroot/pgpool/pgpool-II/pool_auth.c,v 1.4 2007/10/30 01:53:39 y-asaba Exp $
+ * $Header: /cvsroot/pgpool/pgpool-II/pool_auth.c,v 1.5 2007/10/30 02:11:16 y-asaba Exp $
  *
  * pgpool: a language independent connection pool server for PostgreSQL 
  * written by Tatsuo Ishii
@@ -174,7 +174,7 @@ int pool_do_auth(POOL_CONNECTION *frontend, POOL_CONNECTION_POOL *cp)
 	{
 		if (NUM_BACKENDS > 1)
 		{
-			pool_send_error_message(frontend, 3, AUTHFAIL_ERRORCODE,
+			pool_send_error_message(frontend, protoMajor, AUTHFAIL_ERRORCODE,
 									"MD5 authentication is unsupported in replication, master-slave and parallel modes.",
 									"",
 									"check pg_hba.conf",
