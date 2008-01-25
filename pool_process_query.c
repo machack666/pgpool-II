f7f1393bf5eb66cd7b66b54b8213f9dfbfae8635
y-asaba <y-asaba>
2008-01-25 07:20:00 +0000

    Load balancing node was changed after processing transaction. For example,
      SELECT 1; <-- node 1
      BEGIN;
      SELECT 2; <-- node 0(master)
      END;
      SELECT 3; <-- node 0(master)
    
    "SELECT 3" was sended to a master node, not a load balancing node.
    
    VALID_BACKEND macro does not check node validation if
    in_load_balancing is 1. So we do not use VALID_BACKEND macro to check
    whether load balancing node is down.

--- pool_process_query.c
+++ pool_process_query.c
@@ -1,6 +1,6 @@
 /* -*-pgsql-c-*- */
 /*
- * $Header: /cvsroot/pgpool/pgpool-II/pool_process_query.c,v 1.88 2008/01/19 09:49:54 y-asaba Exp $
+ * $Header: /cvsroot/pgpool/pgpool-II/pool_process_query.c,v 1.89 2008/01/25 07:20:00 y-asaba Exp $
  *
  * pgpool: a language independent connection pool server for PostgreSQL 
  * written by Tatsuo Ishii
@@ -320,7 +320,11 @@ POOL_STATUS pool_process_query(POOL_CONNECTION *frontend,
 
 			num_fds = 0;
 
-			if (!VALID_BACKEND(backend->info->load_balancing_node))
+			/*
+			 * Do not use VALID_BACKEND macro.
+			 * Because if in_load_balance == 1, VALID_BACKEND macro may return 0.
+			 */
+			if (BACKEND_INFO(backend->info->load_balancing_node).backend_status == CON_DOWN)
 			{
 				/* select load balancing node */
 				backend->info->load_balancing_node = select_load_balancing_node();
